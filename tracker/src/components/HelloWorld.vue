32fbd1430bcafc1b96dc663e3a06eff9b6a4f4ed
Joel Lord <joelphy@gmail.com>
2018-03-16 10:06:41 -0400

    Up and running

--- tracker/src/components/HelloWorld.vue
+++ tracker/src/components/HelloWorld.vue
@@ -85,10 +85,10 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: "Welcome to Your Vue.js App",
     };
   },
 };
