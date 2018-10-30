2cba81438301e04923c7e54fce0a4f52fe4b4b43
Joel Lord <joelphy@gmail.com>
2018-10-30 07:58:43 +0000

    Added a console.log to the api for testing purposes

--- server.js
+++ server.js
@@ -373,6 +373,7 @@ app.post("/api/user", authCheck, (req, res) => {
 });
 
 app.get("/api/meetups", authCheck, (req, res) => {
+  console.log("User requested list of meetups");
   getMongoUserId(req.headers).then(id => {
     return models.Meetup.find({userId: id, status: {$in: [models.const.MEETUP_STATUS.APPLIED, models.const.MEETUP_STATUS.CONFIRMED]}}).populate("userId");
   }).then(meetups => {
