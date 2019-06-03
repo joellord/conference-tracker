5c5771f20a25f8ac9c4a72d08f6c9dd393033d80
Joel Lord <joelphy@gmail.com>
2019-06-03 08:46:27 -0400

    First version of my own tracker

---
+++ tracker/src/utils/credentials.js
@@ -0,0 +1,7 @@
+export default {
+  AUDIENCE: "http://conf-tracker.com",
+  DOMAIN: "conf-tracker.auth0.com",
+  CLIENT_ID: "wSC1eR3087oo6wKt43TsNtWpbANLVBK8",
+  REDIRECT: `${location.origin}/callback`,
+  SCOPE: "openid profile email"
+};
